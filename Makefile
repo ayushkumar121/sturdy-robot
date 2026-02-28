c110d403298c333236139848fcd6ab925a53b674
ayush.kumar <ayush.kumar@mobikwik.com>
2026-03-01 03:14:19 +0530

    bundling macos app

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ LIB_FREETYPE_LIBS=$(shell pkg-config --libs freetype2)
 UNAME_S := $(shell uname -s)
 
 ifeq ($(UNAME_S),Darwin)
-    CXX_LIBS=vender/glfw/lib-arm64mac/libglfw3.a -framework Cocoa -framework IOKit -framework CoreVideo -framework OpenGL
+    CXX_LIBS=-Lvender/glfw/lib-arm64mac -lglfw3 -framework Cocoa -framework IOKit -framework CoreVideo -framework OpenGL
 else
     CXX_LIBS=-lglfw -lGL -lm
 endif
@@ -15,6 +15,15 @@ endif
 SRC := main.cpp Basic.cpp Texture.cpp TextureLibrary.cpp Shader.cpp ShaderLibrary.cpp QuadMesh.cpp Renderer.cpp Gui.cpp Font.cpp FontLibrary.cpp TextRenderer.cpp StoryEngine.cpp TaskEngine.cpp Game.cpp
 
 all: $(PROJECT_NAME)
+
+$(PROJECT_NAME).app: $(PROJECT_NAME)
+	rm -rf $(PROJECT_NAME).app
+	mkdir -p $(PROJECT_NAME).app/Contents/MacOS
+	mkdir -p $(PROJECT_NAME).app/Contents/Resources
+
+	cp $(PROJECT_NAME) $(PROJECT_NAME).app/Contents/MacOS/
+	cp -R assets $(PROJECT_NAME).app/Contents/Resources/
+	cp Info.plist $(PROJECT_NAME).app/Contents
 
 $(PROJECT_NAME): $(SRC) gl.o stb_image.o
 	$(CXX) -o $(PROJECT_NAME) $(SRC) gl.o stb_image.o $(CXX_FLAGS) $(CXX_LIBS) $(LIB_FREETYPE_FLAGS) $(LIB_FREETYPE_LIBS)
--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
+#include <string_view>
 #include <iostream>
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
+#include <filesystem>
+#ifdef _WIN32
+    #include <windows.h>
+#elif __APPLE__
+    #include <mach-o/dyld.h>
+    #include <vector>
+#else
+    #include <unistd.h>
+    #include <limits.h>
+#endif
 
 #include "Game.h"
 
@@ -9,11 +20,13 @@ void errorCallback(int error, const char *description);
 void debugMessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar * message,
                           const void * user_param);
 void updateFrame(GLFWwindow *window);
+void changeWorkingDir(std::string_view exe_name);
 
 static Game* game;
 
-int main() {
-    // TODO: Abtract away window creation
+int main(int argc, char** argv) {
+    changeWorkingDir(argv[0]);
+
     glfwSetErrorCallback(errorCallback);
     glfwInit();
 
@@ -31,13 +44,13 @@ int main() {
     glfwMakeContextCurrent(window);
 
     if (!gladLoadGL(glfwGetProcAddress)) {
-        std::cout << "Failed to initialize GLAD" << std::endl;
+        std::cerr << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    std::cout << "GL_VERSION: " << glGetString(GL_VERSION) << std::endl;
-    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
-    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cerr << "GL_VERSION: " << glGetString(GL_VERSION) << std::endl;
+    std::cerr << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cerr << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
 
     if (glDebugMessageCallback) {
         glEnable(GL_DEBUG_OUTPUT);
@@ -75,7 +88,7 @@ void errorCallback(int error, const char *description) {
 }
 
 void debugMessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *user_param) {
-    std::cout <<"GL Message: " << message << std::endl;
+    std::cerr <<"GL Message: " << message << std::endl;
 }
 
 void updateFrame(GLFWwindow *window) {
@@ -84,4 +97,39 @@ void updateFrame(GLFWwindow *window) {
     glClear(GL_COLOR_BUFFER_BIT);
     game->render(window);
     glfwSwapBuffers(window);
+}
+
+std::filesystem::path getExecutablePath() {
+#ifdef _WIN32
+    char buf[MAX_PATH];
+    GetModuleFileNameA(nullptr, buf, MAX_PATH);
+    return std::filesystem::canonical(buf);
+#elif __APPLE__
+    uint32_t size = 0;
+    _NSGetExecutablePath(nullptr, &size);
+    std::vector<char> buf(size);
+    _NSGetExecutablePath(buf.data(), &size);
+    return std::filesystem::canonical(buf.data());
+#else
+    char buf[PATH_MAX];
+    ssize_t count = readlink("/proc/self/exe", buf, PATH_MAX);
+    return std::filesystem::canonical(std::string(buf, count));
+#endif
+}
+
+void changeWorkingDir(std::string_view exe_name) {
+    try {
+        std::filesystem::path exePath = getExecutablePath();
+        std::filesystem::path targetDir;
+        std::string exeStr = exePath.string();
+        if (exeStr.find(".app") != std::string::npos) {
+            targetDir = exePath.parent_path() / "Resources";
+        } else {
+            targetDir = exePath.parent_path();
+        }
+        std::cerr << "Changing directory to" << targetDir << std::endl;
+        std::filesystem::current_path(targetDir);
+    } catch (std::filesystem::filesystem_error& e) {
+        std::cerr << "Error changing path: " << e.what() << "\n";
+    }
 }
